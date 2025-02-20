5047fff1f3ff92e157404ad197905d4969ef57cf
mandy <mandyhale10@gmail.com>
2025-02-20 15:41:49 +1300

    model: add required and range data annotations

--- Models/ExpenseModel.cs
+++ Models/ExpenseModel.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace finance_app.Models;
 
 public class ExpenseModel
 {
   public int Id { get; set; }
+  [Required]
   public string Description { get; set; } = null!;
+  [Required]
+  [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
   public double Amount { get; set; }
+  [Required]
   public string Category { get; set; } = null!;
   public DateTime Date { get; set; } = DateTime.Now;
 }
